55f0af0365827f1e4f85e1dd26ceb8d289efb0fa
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-25 23:14:52 +0300

    replace path

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { EnvConfigOptions } from './configs'
+import { EnvConfigOptions } from '@config'
 
 @Module({
 	imports: [ConfigModule.forRoot(EnvConfigOptions)]
