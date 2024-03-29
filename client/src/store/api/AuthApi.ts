7028a7133ddc84645b9141b57fa0c4b1a352c202
vtesliuk <109725756+Lordpluha@users.noreply.github.com>
2024-03-29 20:31:11 +0100

    Update AuthApi.ts
    - change authlogin to authrefresh

--- client/src/store/api/AuthApi.ts
+++ client/src/store/api/AuthApi.ts
@@ -11,6 +11,7 @@ import { LoginUserDto, TLoginResponse, authSlice } from '@store/index'
 const baseQuery = fetchBaseQuery({
 	baseUrl: `${import.meta.env.VITE_SERVER_URL}/api/auth`,
 	credentials: 'same-origin',
+
 	// Automatically use token in authorization header if it provided
 	prepareHeaders: headers => {
 		const token = localStorage.getItem('token')
@@ -30,22 +31,9 @@ const baseQueryWithReauth: BaseQueryFn<
 	FetchBaseQueryError
 > = async (args, api, extraOptions) => {
 	let result = await baseQuery(
-		(args && args.url != 'refresh') ? args : {
-			method: 'POST',
-			url: 'login',
-			credentials: 'same-origin',
-			headers: {
-				Authorization: `Bearer ${localStorage.getItem('token')}`,
-				'Content-Type': 'application/json'
-			}
-		},
-		api,
-		extraOptions
-	)
-
-	if (result.error && result.error.status === 401) {
-		const refreshResult = await baseQuery(
-			{
+		args
+			? args
+			: {
 				url: 'refresh',
 				credentials: 'same-origin',
 				headers: {
@@ -53,33 +41,12 @@ const baseQueryWithReauth: BaseQueryFn<
 					'Content-Type': 'application/json'
 				}
 			},
-			api,
-			extraOptions
-		)
+		api,
+		extraOptions
+	)
 
-		if (refreshResult.data?.accessToken) {
-			localStorage.setItem(
-				'token',
-				refreshResult.data?.accessToken as string
-			)
-			api.dispatch(authSlice.actions.setUser(refreshResult.data.profile))
-			result = await baseQuery(
-				{
-					method: 'POST',
-					url: 'login',
-					credentials: 'same-origin',
-					headers: {
-						Authorization: `Bearer ${localStorage.getItem('token')}`,
-						'Content-Type': 'application/json'
-					}
-				},
-				api,
-				extraOptions
-			)
-			api.dispatch(authSlice.actions.setToken(result.data.accessToken))
-		} else {
-			api.dispatch(authSlice.actions.logout())
-		}
+	//  && result.error.status === 401
+	if (result.error) {
 	}
 	return result
 }
