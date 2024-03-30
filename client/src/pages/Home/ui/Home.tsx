9e75e46806d4f0d2631df59a1c4a284289e70830
Amir <turgunovamir9@gmail.com>
2024-03-30 21:09:49 +0800

    fix

--- client/src/pages/Home/ui/Home.tsx
+++ client/src/pages/Home/ui/Home.tsx
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-import { ListOfClients } from '@widgets/ListOfClients'
-
-const Home = () => {
-	return (
-		<div>
-			<ListOfClients />
-		</div>
-	)
-=======
 import { useAppSelector } from '@store/index'
 
 const Home = () => {
 	const role = useAppSelector(store => store['auth/slice'].user!.role)
 
 	return <div>{role && <h1>Your role {role}</h1>}</div>
->>>>>>> 0b1603ceca5e3cd53a04e144366a1bb3571870e9
 }
 
 export default Home
