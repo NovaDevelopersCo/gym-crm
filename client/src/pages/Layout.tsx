8f8a10ceb78e697f1adbe67040dd1d0cc423fdbe
vtesliuk <109725756+Lordpluha@users.noreply.github.com>
2024-03-31 11:36:52 +0200

    Fix positionation of sidebar

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,7 +1,16 @@
 import { Outlet } from 'react-router-dom'
+import { Sidebar } from '@widgets/Sidebar';
+import styles from './Layout.module.scss'
 
 const Layout = () => {
-	return <Outlet />
+	return <div className={styles.layout}>
+		<div className={styles.horizontal__wrapper}>
+			<Sidebar />
+			<main className={styles.outlet__wrapper}>
+				<Outlet />
+			</main>
+		</div>
+	</div>
 }
 
 export default Layout
