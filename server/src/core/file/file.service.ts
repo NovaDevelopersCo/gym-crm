3930f46fb897380b9f6fb7a665d215503bd29d72
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-31 23:24:19 +0300

    file module

---
+++ server/src/core/file/file.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common'
+import { CloudinaryService } from '../cloudinary/cloudinary.service'
+import { randomUUID } from 'crypto'
+
+@Injectable()
+export class FileService {
+	constructor(private cloudinaryService: CloudinaryService) {}
+
+	// * image: base64 string
+	async uploadBase64(image: string) {
+		const fileName = randomUUID()
+		return await this.cloudinaryService.uploadFile(image, fileName, 'test')
+	}
+
+	async uploadFile(file: Express.Multer.File) {
+		const fileName = randomUUID()
+		return await this.cloudinaryService.uploadFilePipe(file.buffer, fileName, 'test')
+	}
+
+	async remove(path: string) {
+		return await this.cloudinaryService.remove(path)
+	}
+}
