59ecfb5b6ef87858f910f176ffc9e382b51f4463
vtesliuk <109725756+Lordpluha@users.noreply.github.com>
2024-03-31 14:18:45 +0200

    Create LogoutBtn feature component

---
+++ client/src/features/Auth/ui/LogoutBtn.tsx
@@ -0,0 +1,15 @@
+import { authApi } from "@store/api"
+import { useAppDispatch } from "@store/lib"
+import { FC, HTMLAttributes } from "react"
+
+const LogoutBtn: FC<HTMLAttributes<HTMLButtonElement>> = props => {
+	const dispatch = useAppDispatch()
+	return (
+		<button {...props} onClick={(e) => {
+			if (props.onClick) props.onClick(e)
+			dispatch(authApi.endpoints.logoutUser.initiate(''))
+		}}>Logout</button>
+	)
+}
+
+export default LogoutBtn
