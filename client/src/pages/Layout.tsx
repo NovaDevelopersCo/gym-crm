9ba294158eb7a1994bd33fcc6aea803363c201b9
vtesliuk <109725756+Lordpluha@users.noreply.github.com>
2024-03-31 11:46:24 +0200

    Fix layout positionation for topbar

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import { Sidebar } from '@widgets/Sidebar';
 import styles from './Layout.module.scss'
+import { Topbar } from '@widgets/Topbar';
 
 const Layout = () => {
 	return <div className={styles.layout}>
 		<div className={styles.horizontal__wrapper}>
 			<Sidebar />
-			<main className={styles.outlet__wrapper}>
-				<Outlet />
-			</main>
+			<div className={styles.vertical__wrapper}>
+				<Topbar />
+				<main className={styles.outlet__wrapper}>
+					<Outlet />
+				</main>
+			</div>
 		</div>
 	</div>
 }
