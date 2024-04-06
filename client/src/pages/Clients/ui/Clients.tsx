619c59c1b896bddf8ad62136f3a0e27f87e5bd10
vtesliuk <109725756+Lordpluha@users.noreply.github.com>
2024-04-06 15:50:01 +0200

    Update Clients.tsx
    - add clients filter to clients page

--- client/src/pages/Clients/ui/Clients.tsx
+++ client/src/pages/Clients/ui/Clients.tsx
@@ -1,10 +1,12 @@
 import { useAppSelector } from '@store/index'
 import { NewClient } from '@widgets/Client'
 import { ListOfClients } from './../../../widgets/ListOfClients/index';
+import { ClientsFilter } from '@widgets/ClientsFilter';
 
 const ClientsPage = () => {
 	const user = useAppSelector(state => state['auth/slice'].user!)
 	return <>
+		<ClientsFilter />
 		{user.role != 'trainer' && <NewClient />}
 		<ListOfClients />
 	</>
