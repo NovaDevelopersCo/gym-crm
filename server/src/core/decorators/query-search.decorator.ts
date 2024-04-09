d1af104885b1768ca2b6d2c9c8441e82aee29887
coldnanee <pmihaylov036@gmail.com>
2024-04-09 20:53:11 +0300

    fix swagger && query params validate decorator

--- server/src/core/decorators/query-search.decorator.ts
+++ server/src/core/decorators/query-search.decorator.ts
@@ -1,12 +1,26 @@
 import { applyDecorators } from '@nestjs/common'
 import { IsEnum, IsOptional } from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
+import { QuerySearchValidate } from './query-search-validate.decorator'
+
+import type { TQuerySearchValidatorObj } from '@/core/types'
 
 // * for swagger
-export const QuerySearch = (eList: object, description: string, message: string) => {
-	return applyDecorators(
+export const QuerySearch = <T extends string = ''>(
+	eList: object,
+	description: string,
+	message: string,
+	validator?: TQuerySearchValidatorObj<T>
+) => {
+	const decorators = [
 		ApiProperty({ required: false, description, enum: eList }),
 		IsOptional(),
 		IsEnum(eList, { message })
-	)
+	]
+
+	if (validator) {
+		decorators.push(QuerySearchValidate<T>(validator))
+	}
+
+	return applyDecorators(...decorators)
 }
