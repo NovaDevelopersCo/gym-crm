6ea496fa36716ab599ae6918da037ddf6dd4f700
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-04-13 15:30:39 +0300

    staff module

--- server/src/modules/staff/staff.controller.ts
+++ server/src/modules/staff/staff.controller.ts
@@ -1,23 +1,75 @@
-import { Body, Controller, UsePipes, ValidationPipe, Post } from '@nestjs/common'
-import { CreateDto } from './dto'
+import {
+	Body,
+	Controller,
+	UsePipes,
+	ValidationPipe,
+	Post,
+	Get,
+	Put,
+	Delete,
+	Param,
+	Query,
+	UseInterceptors,
+	ClassSerializerInterceptor
+} from '@nestjs/common'
+import { CreateStaffDto, UpdateStaffDto, FindAllStaffDto } from './dto'
 import { StaffService } from './staff.service'
 import { RolesAuthGuard } from '@/auth/guards/role.guard'
 import { EStaffRole } from '@/core/enums'
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger'
-
 import { StaffDocSwagger } from './swagger'
+import { GetByIdParamsDto } from '@/core/dto'
+import { Staff } from '@/core/decorators'
 
 @ApiTags('Персонал')
+@UseInterceptors(ClassSerializerInterceptor)
 @ApiBearerAuth('access-token')
 @UsePipes(new ValidationPipe({ whitelist: true }))
-@RolesAuthGuard(EStaffRole.DIRECTOR)
 @Controller('staff')
 export class StaffController {
 	constructor(private readonly staffService: StaffService) {}
 
+	@RolesAuthGuard(EStaffRole.DIRECTOR)
 	@StaffDocSwagger.create()
 	@Post()
-	create(@Body() dto: CreateDto) {
+	create(@Body() dto: CreateStaffDto) {
 		return this.staffService.create(dto)
+	}
+
+	// ! Продумать роли доступа
+	@RolesAuthGuard(EStaffRole.DIRECTOR, EStaffRole.ADMIN)
+	@StaffDocSwagger.getAll()
+	@Get()
+	getAll(@Query() query: FindAllStaffDto) {
+		return this.staffService.getAll(query)
+	}
+
+	// ! Продумать роли доступа
+	@RolesAuthGuard(EStaffRole.DIRECTOR, EStaffRole.ADMIN)
+	@StaffDocSwagger.getById()
+	@Get(':id')
+	getOne(@Param() { id }: GetByIdParamsDto) {
+		return this.staffService.getById(id, true)
+	}
+
+	// !checked
+	@RolesAuthGuard(EStaffRole.DIRECTOR, EStaffRole.ADMIN)
+	@StaffDocSwagger.update()
+	@Put(':id')
+	update(
+		@Param() { id }: GetByIdParamsDto,
+		@Body() dto: UpdateStaffDto,
+		@Staff('id') staffId: number,
+		@Staff('role') role: EStaffRole
+	) {
+		return this.staffService.update(id, dto, staffId, role)
+	}
+
+	// ! Продумать роли доступа
+	@RolesAuthGuard(EStaffRole.DIRECTOR)
+	@StaffDocSwagger.delete()
+	@Delete(':id')
+	delete(@Param() { id }: GetByIdParamsDto) {
+		return this.staffService.delete(id)
 	}
 }
--- server/src/modules/staff/staff.service.ts
+++ server/src/modules/staff/staff.service.ts
@@ -2,8 +2,8 @@ import { StaffEntity } from '@/modules/staff/entities'
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { hash } from 'bcrypt'
-import { Repository } from 'typeorm'
-import { CreateDto } from './dto'
+import { ILike, Repository } from 'typeorm'
+import { CreateStaffDto, FindAllStaffDto, UpdateStaffDto } from './dto'
 import { EStaffRole } from '@/core/enums'
 
 @Injectable()
@@ -12,18 +12,20 @@ export class StaffService {
 		@InjectRepository(StaffEntity) private readonly staffRepository: Repository<StaffEntity>
 	) {}
 
-	async getById(staffId: number, withError?: boolean) {
-		const staff = await this.staffRepository.findOne({ where: { id: staffId } })
+	async getById(id: number, withError?: boolean) {
+		const staff = await this.staffRepository.findOne({
+			where: { id },
+			relations: { club: true }
+		})
 
 		if (withError && !staff) {
-			throw new NotFoundException(`Управляющий с id: ${staffId} не найден`)
+			throw new NotFoundException(`Управляющий с id: ${id} не найден`)
 		}
-
-		const { fio, email, role, groups, club, id } = staff
-
-		return { fio, email, role, groups, club, id }
+		const { fio, email, role, club } = staff
+		return { fio, email, role, club, id }
 	}
 
+	// ! ???
 	async getByEmail(email: string) {
 		const staff = await this.staffRepository.findOne({ where: { email } })
 
@@ -34,7 +36,7 @@ export class StaffService {
 		return staff
 	}
 
-	async create({ password, ...data }: CreateDto) {
+	async create({ password, ...data }: CreateStaffDto) {
 		if (data.role === 'director') {
 			throw new BadRequestException(
 				'Нельзя создать более одного аккаунта управляющего директора'
@@ -48,12 +50,65 @@ export class StaffService {
 		}
 
 		const hashPassword = await hash(password, 7)
+		const savedStaff = await this.staffRepository.save({ ...data, password: hashPassword })
 
-		const savedUser = await this.staffRepository.save({ ...data, password: hashPassword })
+		const { id, email, role, fio } = savedStaff
+		return { id, email, role, fio }
+	}
 
-		const { email, role, id } = savedUser
+	async getAll({ sortBy, count, page, q, searchBy, sortOrder }: FindAllStaffDto) {
+		const [items, total] = await this.staffRepository.findAndCount({
+			order: {
+				[sortBy]: sortOrder
+			},
+			where: {
+				[searchBy]: ILike(`%${q}%`)
+			},
+			take: count,
+			skip: page * count - count,
+			relations: {
+				club: true
+			}
+		})
 
-		return { email, role, id }
+		// ! replace
+		return {
+			items,
+			meta: {
+				total
+			}
+		}
+	}
+
+	async update(id: number, dto: UpdateStaffDto, staffId: number, role: EStaffRole) {
+		if (staffId !== id && role !== EStaffRole.DIRECTOR) {
+			throw new BadRequestException('Вы не можете менять данные чужого пользователя')
+		}
+
+		const staff = await this.getById(id, true)
+
+		if (staff.email !== dto.email) {
+			const oldStaff = await this.staffRepository.findOne({ where: { email: dto.email } })
+			if (oldStaff) {
+				throw new BadRequestException(`Персонал с email ${dto.email} уже существует`)
+			}
+			staff.email = dto.email
+		}
+
+		if (staff.fio === dto.fio) staff.fio = dto.fio
+
+		const savedStaff = await this.staffRepository.save({ ...staff })
+		const { email, fio, role: staffRole } = savedStaff
+		return { id, email, fio, role: staffRole }
+	}
+
+	//! Обсудить
+	async delete(id: number) {
+		const staff = await this.getById(id, true)
+		if (staff.role === EStaffRole.DIRECTOR) {
+			throw new BadRequestException('Нельзя удалить персонал с ролью director')
+		}
+		await this.staffRepository.delete({ id })
 	}
 
 	async checkRole(id: number, role: EStaffRole) {
