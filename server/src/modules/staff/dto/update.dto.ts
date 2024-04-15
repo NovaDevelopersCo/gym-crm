03e78bcfece9463c9f67ce36de7d2e4352b105bf
coldnanee <145275662+coldnanee@users.noreply.github.com>
2024-04-15 16:58:34 +0300

    Update update.dto.ts

--- server/src/modules/staff/dto/update.dto.ts
+++ server/src/modules/staff/dto/update.dto.ts
@@ -1,4 +1,4 @@
 import { OmitType } from '@nestjs/swagger'
 import { CreateStaffDto } from './create.dto'
 
-export class UpdateStaffDto extends OmitType(CreateStaffDto, ['password', 'role'] as const) {}
+export class UpdateStaffDto extends OmitType(CreateStaffDto, ['password', 'role']) {}
