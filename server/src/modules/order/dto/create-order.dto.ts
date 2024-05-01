6e5e58ac9ef8d35fcfe89bb44cfae7624d773cd2
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-05-02 00:57:22 +0300

    dto order

---
+++ server/src/modules/order/dto/create-order.dto.ts
@@ -0,0 +1,18 @@
+import { CommonDecoratorsSwagger } from '@/core/swagger'
+import { OrderDecoratorsSwagger } from '../swagger'
+
+export class ProductWithCount {
+	@CommonDecoratorsSwagger.id(true)
+	id: number
+
+	@OrderDecoratorsSwagger.count(true)
+	count: number
+}
+
+export class CreateOrderDto {
+	@CommonDecoratorsSwagger.userId(true)
+	user: number
+
+	@OrderDecoratorsSwagger.productCountObject(true)
+	products: ProductWithCount[]
+}
---
+++ server/src/modules/order/dto/find-all.dto.ts
@@ -0,0 +1,19 @@
+import { FullQueryDto } from '@/core/dto'
+import { QuerySearch } from '@/core/decorators'
+import { IsInt, IsOptional } from 'class-validator'
+import { Type } from 'class-transformer'
+
+enum ESort {
+	CREATE_DATE = 'createDate',
+	TOTAL = 'total'
+}
+
+export class FindAllOrderDto extends FullQueryDto {
+	@QuerySearch(ESort, 'Сортировка по', "Параметр 'Сортировка по' невалиден")
+	sortBy: ESort = ESort.CREATE_DATE
+
+	@IsOptional()
+	@Type(() => Number)
+	@IsInt()
+	user: number
+}
