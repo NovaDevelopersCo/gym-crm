7fa709a7140f46d00f108202a0f35fb3c1662319
Amir <turgunovamir9@gmail.com>
2024-05-01 21:22:52 +0800

    small change

--- client/src/features/EditGroup/ui/EditGroupBtn.tsx
+++ client/src/features/EditGroup/ui/EditGroupBtn.tsx
@@ -1,22 +1,33 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import { Button } from '@/shared'
 import { IGroup } from '@/store'
 import { ButtonProps } from 'antd'
+
+import { EditGroupModal } from '@entities/EditGroupModal'
 
 type EditGroupBtnProps = {
 	groupId: IGroup['id']
 } & ButtonProps
 
 const EditGroupBtn: FC<EditGroupBtnProps> = ({ groupId, ...props }) => {
+	const [isModalOpen, setIsModalOpen] = useState(false)
+
+	const handleEdit = () => {
+		console.log(`Edit group with id: ${groupId}`)
+		setIsModalOpen(prev => !prev)
+	}
+
 	return (
-		<Button
-			onClick={() => console.log(`Edit group with id: ${groupId}`)}
-			{...props}
-			type='dashed'
-		>
-			Изменить
-		</Button>
+		<>
+			<Button onClick={handleEdit} {...props} type='dashed'>
+				Изменить
+			</Button>
+			<EditGroupModal
+				isModalOpen={isModalOpen}
+				setIsModalOpen={setIsModalOpen}
+			/>
+		</>
 	)
 }
 
